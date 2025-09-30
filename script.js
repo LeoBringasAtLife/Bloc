5337e41fe47814b4ba412ad2b3972a3c3e0f52d9
Leonardo Bringas <leonardoisaiasbringas34@gmail.com>
2025-09-29 21:29:56 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 const devocionales = [
     {
-        id: 1,
+        id: 5,
+        titulo: "Prueba y error",
+        referencia: "Proverbios 29:15",
+        fecha: "29/09/2025",
+        imagen: "imagenes/prueba-y-error.webp",
+        contenido: `
+        <p>Los tiempos cambian, sí. El tiempo pasado no fue mejor, solo fue pasado; pero es verdad que los tiempos cambian. Hay conceptos de la pedagogía moderna con los que “no la voy”. El problema es que decir esto me pone en la vereda de: “en tu época”, “anticuado”, “ahora no es tan así”. O peor, mucho peor: “patriarcado”.</p>
+
+        <p>Me trataron de “machirulo patriarcal” en Facebook por opinar que la legislación que permite optar por el apellido que llevan los hijos es una idiotez. Me acusaron de misógino porque, según ellos, yo estaba avalando la opresión machista sobre la mujer.</p>
+
+        <p>Cuando lo único que dije (o quise decir) es que hacer eso traería infinitos conflictos a la hora de las genealogías, sucesiones y herencias.</p>
+
+        <p>¿Te imaginás un Pérez hijo de un López, hijo de un González, y este hijo de un Fernández?</p>
+
+        <p>Los tiempos cambian y no para mejor. Hay cosas que no pueden cambiar, así como otras sí deben hacerlo. Amo la tecnología con la que convivimos actualmente y siento un poco de melancolía de no llegar a conocer la de los próximos mil años.</p>
+
+        <p>El ser humano tiene una computadora en su cerebro que debe ser programada y puesta a prueba. Una persona sin una formación racional es solo un manojo de emociones o respuestas intuitivas a los acontecimientos; o sea… un animal.</p>
+
+        <p>La educación de las emociones es vital para el desarrollo neurocognitivo y las relaciones interpersonales.</p>
+
+        <p>Lamentablemente, hoy vemos abundar a pasos agigantados las crisis de ansiedad, ataques de pánico y aislamiento social.</p>
+
+        <p>Los pedagogos modernos buscan cuidar el alma, cuando deberían cuidar la mente, y evitan todo tipo de confrontación o exposición al conflicto y al fracaso, para que nadie salga lastimado.</p>
+
+        <p>Pero para evitar que el nene se caiga de la bici, no hay que prohibirle subir a la bici, sino enseñarle —aun a costa de golpes, raspones y heridas— a dominar esa bici.</p>
+
+        <p>Reemplazá “bici” por cualquier situación que imagines, y la respuesta y el resultado son los mismos.</p>
+
+        <p>Vivimos en una tremenda carencia de “educación en inteligencia emocional”, que da como resultado lo opuesto a lo que pretendían lograr: corazones heridos, jóvenes frágiles que se convertirán en adultos débiles y toda una generación de “cristalitos” fácilmente ofendidos.</p>
+
+        <p>Dice Proverbios 29:15: “La vara y la corrección dan sabiduría; mas el muchacho consentido avergonzará a su madre.”</p>
+
+        <p>Es difícil que una madre se sienta avergonzada por un hijo ¿no? Normalmente tienden a justificar, “entender” y aun apañar todo. Pero parece que es posible: cuando no se enseña para la vida y no se corrigen los errores; cuando se evita tanto el raspón que acaba rompiéndose la cabeza al caer de la bici a la que se subió a escondidas.</p>
+
+        <p>Sin darnos cuenta, lo mismo pasa con la iglesia. ¡Cierto! La iglesia está compuesta de personas, y esos adultos débiles, consecuencia de juventudes frágiles, terminan siendo siervos y ministros inestables, con reacciones emocionales y rápidamente ofensibles.</p>
+
+        <p>Necesitan ser corregidos, necesitan ser enseñados, pero no aceptan ser señalados en su error, cuando Salomón fue muy claro al decir:</p>
+
+        <p>“El siervo no se corrige con palabras; Porque entiende, mas no hace caso.” (Proverbios 29:19)</p>
+
+        <p>Hoy me preguntaron hacia dónde nos lleva esta nueva época en la que Dios nos introdujo por medio de sus enviados. Y todavía no lo sé, pero sí sé que debemos corregir el rumbo.</p>
+
+        <p>Podemos pecar en pensar que no necesitamos ser enseñados y corregidos. Si así creemos, no somos más que unos pobres cachorritos. La vida consiste en formación, edificación, proceso, prueba y error.</p>
+
+        <p>Es más, Pablo lo demuestra con el texto que usamos para hablar de las promesas de Dios:</p>
+
+        <p>“…el que comenzó en ustedes la buena obra, la perfeccionará hasta el día de Jesucristo.” (Filipenses 1:6)</p>
+
+        <p>Lo que debe ser perfeccionado es porque no está terminado ¿no? Lo que no está terminado, necesita ser formado, corregido, probado ¿no?</p>
+
+        <p>Dios va a terminar la obra que empezó en vos, porque todavía estás en plena formación.</p>
+
+        <p>Una nueva época comienza…</p>
+
+        <p>No te quedes afuera…</p>
+    `
+    },
+    {
+        id: 4,
         titulo: "Una nueva época",
         referencia: "Isaías 54:2",
         fecha: "28/09/2025",
@@ -31,46 +89,183 @@ const devocionales = [
         <p>Una nueva época. No te quedes atrás.</p>
     `
     },
-    {
-        id: 2,
-        titulo: "Titulo 2",
-        referencia: "Pasaje 1:1",
-        fecha: "27/09/2025",
-        imagen: "imagenes/imagen.webp",
-        contenido: `
-            <p>Texto</p>
-        `
-    },
+
     {
         id: 3,
-        titulo: "Titulo 3",
-        referencia: "Pasaje 1:1",
-        fecha: "26/09/2025",
-        imagen: "imagenes/imagen.webp",
+        titulo: "Especiales…",
+        referencia: "Isaías 40:31",
+        fecha: "27/09/2025",
+        imagen: "imagenes/especiales.webp",
         contenido: `
-            <p>Texto</p>
+        <p>Y Dios nos sigue hablando… cambio de visión, cambio de enfoque. Una nueva temporada (spoiler de mañana) que está comenzando, una nueva etapa a transitar.</p>
+
+        <p>Realmente sí, como te dije ayer (¿lo leíste?), somos privilegiados. No es una exageración ni manipulación emocional. Así como la publi de la American Express dice que “pertenecer tiene sus privilegios”, o el viejo institucional de Canal 13 decía: “ser parte es bueno”, asimismo es bueno pertenecer al pueblo de Dios.</p>
+
+        <p>Incluso si lo miramos con una óptica natural. La Biblia dice que hemos sido creados “un poco menor[es] que los ángeles” (Salmo 8:5), y posteriormente cuando habla del hombre espiritual, del que es hijo de Dios, dice que somos “superiores a los ángeles” (1 Corintios 6:3).</p>
+
+        <p>¿Cuál es el factor que hace la diferencia? El espíritu de Dios en nosotros y el derecho otorgado por Dios a ser sus hijos (Romanos 8:9; Juan 1:12).</p>
+
+        <p>“Antes no éramos pueblo” dice Pablo, pero que “ahora somos pueblo de Dios” (1 Pedro 2:10). Pueblo, hijos, elegidos, ungidos… Es un privilegio ser cristiano.</p>
+
+        <p>Pero la falta de identidad y de relación con él nos hace mantener la cabeza gacha, mirando el conflicto, mirando el problema, más preocupados por lo que el enemigo pueda hacer que por avanzar en la obra del Señor.</p>
+
+        <p>Es una comparación sencilla: las águilas surcan el cielo con la cabeza recta o en alto y solo la inclinan levemente para enfocarse en su presa. En cambio la gallina… está mirando el suelo buscando gusanos. ¿Qué dice Dios de vos? ¿Águila o gallina? ¿Gallina o águila? (¡Ay si escuchara Calvin! —Michael J. Fox— Volver al futuro 1,2,3).</p>
+
+        <p>No dudes… “los que esperan en el Señor levantarán alas como…” ¿las gallinas?. No. “¡las águilas!” (Isaías 40:31).</p>
+
+        <p>Me retumban las palabras de Nehemías: “¿un hombre como yo va a huir?” (Nehemías 6:11). Supongo que Isaías las conocía. Pero además de conocerlas Dios volvió a enfocarse en eso cuando dijo, por la boca de Isaías, “Yo mismo soy su consolador. ¿Quién eres tú para tener miedo de hombres mortales, que son como la paja?” (Isaías 51:12).</p>
+
+        <p>Una comparación un poco extravagante. Normalmente el “¿quién sos vos para..?” va seguido de algo que apunte a la arrogancia. “¿Quién sos para sentirte superior, para ponerte encima, para hablar así..?” etc. Pero en este caso es un “¿Quién sos vos para dejarte amedrentar? ¿Para temer del otro? ¿Para dejarte manipular por la acusación?”</p>
+
+        <p>¿Será acaso que creés que Dios no es suficiente? ¿Será que no confiás en él? ¿Será tal vez que no te creas digno? ¿Será que tus miserias te ciegan?</p>
+
+        <p>Sea lo que sea, sientas lo que sientas, lo espiritual no se mide por percepción, sensación o mérito, sino solo por fe (2 Corintios 5:7).</p>
+
+        <p>¡Ay cristiano! No sos más que nadie. 
+        Pero tampoco sos menos. <strong>Sos un pri-vi-le-gia-do…</strong> al que Dios llamó para impactar.</p>
+
+        <p>No te detengas ante las amenazas <br>
+        No hagas caso de las humillaciones <br>
+        No escuches a los profetas del fracaso. <br>
+        No prestes atención a los llamados de la mediocridad <br>
+        </p>
+
+        <p>Dios no te trajo hasta acá… para volver atrás (Hebreos 10:39). <br>
+        —Marcos Witt—
+        </p>
         `
     },
     {
-        id: 4,
-        titulo: "Titulo 4",
-        referencia: "Pasaje 1:1",
-        fecha: "25/09/2025",
-        imagen: "imagenes/imagen.webp",
+        id: 2,
+        titulo: "Privilegios",
+        referencia: "Efesios 4:1",
+        fecha: "29/09/2025",
+        imagen: "imagenes/privilegios.webp",
         contenido: `
-            <p>Texto</p>
-        `
+        <p>Estamos de aniversario. Estamos transitando los 5 días de celebración, como hacemos cada año. Estamos de aniversario… y Dios nos está hablando.</p>
+
+        <p>Creo que a veces no nos damos cuenta del semejante privilegio que tenemos. No, tranquilo, no hay soberbia ni arrogancia, sino todo lo contrario. Insisto, creo que no nos damos cuenta del privilegio que tenemos de que Dios nos hable.</p>
+
+        <p>Por supuesto que Dios no habla para charlar del clima ni para perder tiempo. Con Dios nunca es una charla de ascensor o de sala de espera. Si Dios habla, cuando Dios habla, es para corrección, dirección, edificación y transformación.</p>
+
+        <p>Después de todo, somos hijos, pero somos siervos, sirvientes de Dios.</p>
+
+        <p>Hace algunos días atrás hablamos acerca del propósito, de la función que tenemos por cumplir y el camino a la plenitud. Recuerdo que Jesús dijo que “a quien más se le da, más se le pide” (Lucas 12:48). Bueno, ya sabés: “en el reino de los cielos todo gira alrededor de la siembra y la cosecha”, y no somos los únicos que cosechamos, Dios también espera cosechar de nosotros. ¿No dice acaso que espera que demos fruto? (Juan 15:8). Somos privilegiados y en esa misma dirección debemos responder.</p>
+
+        <p>Dice Efesios 4:1: “les ruego que vivan como es digno del llamamiento que han recibido,…” .</p>
+
+        <p>Siembra y cosecha… una respuesta acorde al nivel del llamado, una cosecha proporcional a la siembra de Dios en vos (sí, en mí también). Uff… con esto solo ya tenemos para masticar y digerir… (y atragantarnos).</p>
+
+        <p>Somos privilegiados. Somos exigidos. Entiendo que se espera de cada creyente algo más de lo que se puede esperar de quien no lo es. Creo que Dios nos pone en una posición superior, pero no para sentirnos más, sino para:</p>
+
+        <p>Ver a todos alrededor…</p>
+
+        <p>Ser vistos por todos alrededor…</p>
+
+        <p>Con un solo fin: servir, afectar, influenciar, transformar, guiar, alumbrar, impactar. (Bueno, era uno solo…).</p>
+
+        <p>Eso nos obliga (¡y dale!) a ser ejemplo, a hablar más con acciones que con palabras, a predicar más con gestos que con versículos, a ser luz para los demás (Mateo 5:14-16). A no vivir como vivíamos antes, sino a vivir según los principios de Dios.</p>
+
+        <p>Hoy escuchaba una nota periodística que resumía que en los últimos 20 años la sociedad en general perdió valores, moral, principios. En definitiva, que el nivel sociocultural decreció notablemente. Me voy a ganar algunos enemigos, pero es algo que lo podés ver en la cultura: música, teatro, arte en general. Hoy prevalece lo “popular”, entendiéndose por popular lo vulgar, lo obsceno, lo casi pornográfico y lo no tan casi.</p>
+
+        <p>¿Escuchaste alguna letra de reguetón o el nuevo cuarteto? No, no me la doy de nada, no soy más que nadie y me crié escuchando y bailando cumbia de Los Wawancó y El Cuarteto Imperial.</p>
+
+        <p>Tenemos que marcar la diferencia. Debemos ser tan cercanos y parecidos como para ser aceptados y tan distintos como para que se note la diferencia. Debemos adaptarnos, sí, también en nuestras formas, prédicas, vestimenta y estilos, para poder encajar, sin amoldarnos, para afectar.</p>
+
+        <p>Dijo Pablo a los efesios (¡aguante la carta a los efesios!): “… esto quiero decirles en el nombre del Señor, y en esto quiero insistir: no vivan ya como la gente sin Dios, que vive de acuerdo a su mente vacía.” (Efesios 4:17). Y termina el concepto diciendo: “…renuévense en el espíritu de su mente,…” (Efesios 4:23).</p>
+
+        <p>Sí, me extendí. Sí, es largo. Es que Dios nos está hablando. Dios nos habló y hay que esforzarnos, prepararnos, ¡adaptarnos! e impactar. ¿Te acordás que el miércoles nos dijo que deberíamos ser “sorprendentes y extravagantes”? Bueno, no se logra con mediocridad.</p>
+
+        <p>¿Estás marcando la diferencia?</p>
+
+        <p>¿Estás subiendo a un nivel superior?</p>
+
+        <p>¿Te das cuenta de lo privilegiado que sos?</p>
+
+        <p>¿Te estás adaptando para encajar?</p>
+
+        <p>¿O te estás amoldando para pertenecer?</p>
+
+        <p>Dios nos está hablando.</p>
+    `
     },
     {
-        id: 5,
-        titulo: "Titulo 5",
-        referencia: "Pasaje 1:1",
-        fecha: "24/09/2025",
-        imagen: "imagenes/imagen.webp",
+        id: 1,
+        titulo: "Escondidas",
+        referencia: "Isaías 45:15",
+        fecha: "29/09/2025",
+        imagen: "imagenes/escondidas.webp",
         contenido: `
-            <p>Texto</p>
-        `
-    },
+        <p>¿Quién no jugó a las escondidas alguna vez? Esa mezcla de adrenalina y ansiedad, tanto por buscar al otro como por no ser encontrado. Las corridas para encontrar un lugar seguro mientras escuchás el: “…el que no se escondió, se embroma.” Los nervios cuando estás cerca del escondite o cuando escuchás un: “¡piedra libre a…!”</p>
+
+        <p>Sí, ya sé que para algunos de los que están leyendo esto es algo de alguna película vieja. Hoy no se juega a las escondidas, y si se esconden… dejémoslo ahí.</p>
+
+        <p>Pero fue una parte importante de nuestra infancia. Los juegos no son solo juegos, sino que te entrenan para la vida.</p>
+
+        <p>¡Qué! ¿Te sueno anticuado? No estoy diciendo que “tiempo pasado fue mejor”, porque es una gran mentira. Estoy diciendo que <b>los juegos te entrenan para la vida.</b></p>
+
+        <p>Habilidades físicas, relaciones interpersonales, interacción con el otro, destrezas varias, etc., que después usarías en otro contexto. ¡Como los animales! ¿Viste cómo los cachorros juegan con sus hermanos de camada o con sus padres, luchando y mordiéndose? ¿Nunca jugaste a la lucha con papá? Bueno… eso.</p>
+
+        <p>Jugar a las escondidas tiene su clímax en el encuentro y la corrida al punto de conteo. El que llega primero gana, y otro te podía salvar.</p>
+
+        <p>Recuerdo también la frustración cuando eso no pasaba, cuando me tocaba ir a la pared: “¡la cuenta, Walter!” o el momento mismo de ser descubierto.</p>
+
+        <p>Pensar que pasan los años, nos volvemos adultos y seguimos haciendo lo mismo. ¿Te acordás de Adán escondiéndose de Dios? “¿Dónde estás?” (Génesis 3:9)</p>
+
+        <p>¡No harás lo mismo vos…! ¿no?</p>
+
+        <p>¿Sabías que Dios se esconde? <b>Dios se esconde.</b></p>
+
+        <p>Como un padre, nos entrena para la vida espiritual.</p>
+
+        <p>Como un juego, nos hace explorar distintas emociones.</p>
+
+        <p>Como un tesoro… se hace buscar.</p>
+
+        <p>Dios le habla a Israel, reprendiéndolos. Claro, acostumbrados a convivir con la gloria de Adonai, fueron perdiendo el interés y la reverencia. Después de todo, ¡eran (son) el pueblo elegido! Dios, creerían ellos, Jehová, Yavé, no era tan solo un Dios, era el <b>Dios de Israel.</b> Así lo nombraban en otros reinos y tal vez… tal vez… Israel llegó a pensar que ellos eran los que le daban nombre a su Dios.</p>
+
+        <p>“Y en verdad tú, Dios y salvador de Israel, eres un Dios que se esconde.” (Isaías 45:15)</p>
+
+        <p>Así habla con Él Isaías, entendiendo que se escondía para provocar la búsqueda. El problema fue que <b>muchos ni siquiera se dieron cuenta de que Dios no estaba visible.</b> Unos pocos (siempre hay un remanente) empezaron a buscarlo con desesperación.</p>
+
+        <p>Creo que de ahí viene esa expresión: <b> “buscar a Dios”.</b></p>
+
+        <p>O de Isaías 55:6: “Búsquen a Dios mientras puede ser hallado,…”</p>
+
+        <p>En definitiva, es el mismo contexto: Dios se escondía de un Israel que lo menospreciaba e ignoraba. Dios se escondía para provocar la búsqueda. Dios se escondía para que, quienes lo amaban y anhelaban, exploten de emoción —llanto, risa o quebranto— al encontrarse con Él.</p>
+
+        <p>Hoy Dios sigue siendo el mismo, aunque obra de otra manera.</p>
+
+        <p>Obra de otra manera, aunque sigue siendo el mismo: “…no hay mudanza, ni sombra de variación.” (Santiago 1:17)</p>
+
+        <p>Te sigue provocando a la búsqueda, se sigue escondiendo para ser hallado.</p>
+
+        <p>Hay otra opción, que depende de vos y tu relación con Él:</p>
+
+        <p>Dios se esconde <b>para que no te acostumbres</b> a la manera en que te encontrabas con Él. Un día, de repente, hacés lo mismo de siempre y Él no viene a la cita. Empezás a buscar y no está.</p>
+
+        <p>¿Qué hacés en ese caso? De tu respuesta depende tu crecimiento espiritual.</p>
+
+        <p>En tu respuesta está la puerta que te lleva a una nueva posición espiritual.</p>
+
+        <p>De eso se trata: una nueva posición espiritual.</p>
+
+        <p>No juegues con Dios.</p>
+
+        <p>Pero prestá atención cuando Él juega a las escondidas con vos.</p>
+
+        <p>No te acostumbres a la rutina ni creas que, si ya no está, es que se fue y que nunca volverá.</p>
+
+        <p>¡Tal vez está detrás de una cortina! (espiá si se ven los zapatos)</p>
+
+        <p>¡Tal vez en la oscuridad! (no te limites a las estructuras religiosas)</p>
+
+        <p>¡Tal vez te sigue de atrás! (no hagas lo obvio, te está invitando a que hagas lo que nunca hiciste)</p>
+
+        <p>Sí, <b>Dios se esconde… pero se deja encontrar.</b></p>
+    `
+    }
+
 ];
 
 // Función para extraer y formatear meses y años
